caf31fec83828433a6259465b7ea6fefe1cbfd49
Erwin Jacobs <erwinpj@outlook.com>
2017-11-21 13:36:15 +0100

    Noodzakelijke commit om te mogen pullen?

--- Project/Project/Debug/StartScreen.c.d
+++
@@ -1,6 +0,0 @@
-C:\Users\wsvdy\AppData\Local\Temp\VMBuilds\Project\uno\Debug\StartScreen.c.o: \
- C:\Users\wsvdy\AppData\Local\Temp\VMBuilds\Project\uno\Debug\StartScreen.c \
- C:\Users\wsvdy\AppData\Local\Temp\VMBuilds\Project\uno\Debug\StartScreen.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
--- Project/Project/Debug/StartScreen.cpp.d
+++
@@ -1,3 +0,0 @@
-C:\Users\wsvdy\AppData\Local\Temp\VMBuilds\Project\uno\Debug\StartScreen.cpp.o: \
- C:\Users\wsvdy\AppData\Local\Temp\VMBuilds\Project\uno\Debug\StartScreen.cpp \
- C:\Users\wsvdy\AppData\Local\Temp\VMBuilds\Project\uno\Debug\StartScreen.h
